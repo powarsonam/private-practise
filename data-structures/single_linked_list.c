7f5d631083f82abec52f84d6fc29e95e7d05be8c
powarsonam <powarsonam@gmail.com>
2013-09-21 11:07:50 +0530

    Update single_linked_list.c

--- data-structures/single_linked_list.c
+++ data-structures/single_linked_list.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+// global definition
+struct node_t
+{
+  int var;
+  struct node_t *next;
+}node;
 
+node * create_list(int );
+node * append_list(int);
+node * delete_list(int);
 int main ()
 {
+// create a linked list
+
+
+// append a list
+
+// delete a list
+
+// insert a list
+
 
 return 1;
 }
